32c09cfb0bdd1834ca5da8053ae827cbb0d2de4b
Quentin Barrand <quentin@quba.fr>
2020-12-08 21:20:43 +0100

    Add a probably dumb implementation of part 2.

--- 08/main.go
+++ 08/main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+var errLoop = errors.New("loop")
+
 type instruction struct {
 	operation string
 	argument  int
@@ -34,6 +36,13 @@ func main() {
 	}
 
 	log.Printf("Part 1: acc: %d", findLoop(instructions))
+
+	acc2, err := fixProgram(instructions, 0, make([]bool, len(instructions)), true)
+	if err != nil {
+		log.Fatal("Could not fix the program :(")
+	}
+
+	log.Printf("Part 2: acc: %d", acc2)
 }
 
 // part 1
@@ -68,4 +77,53 @@ func findLoop(instructions []*instruction) int {
 	}
 
 	return acc
+}
+
+// part 2
+func fixProgram(instructions []*instruction, i int, executed []bool, changeAllowed bool) (int, error) {
+	if executed[i] {
+		return 0, errLoop
+	}
+
+	if i == len(instructions)-1 {
+		return 0, nil
+	}
+
+	executed[i] = true
+
+	switch in := instructions[i]; in.operation {
+	case "acc":
+		n, err := fixProgram(instructions, i+1, cloneSlice(executed), changeAllowed)
+		return in.argument + n, err
+	case "jmp":
+		if changeAllowed {
+			if n, err := fixProgram(instructions, i+1, cloneSlice(executed), false); err == nil {
+				return n, err
+			}
+
+			return fixProgram(instructions, i+in.argument, cloneSlice(executed), true)
+		}
+
+		return fixProgram(instructions, i+in.argument, cloneSlice(executed), false)
+	case "nop":
+		if changeAllowed {
+			if n, err := fixProgram(instructions, i+in.argument, cloneSlice(executed), false); err == nil {
+				return n, err
+			}
+
+			return fixProgram(instructions, i+1, cloneSlice(executed), true)
+		}
+
+		return fixProgram(instructions, i+1, cloneSlice(executed), false)
+	default:
+		return 0, fmt.Errorf("%d: operation %q not understood", i, in.operation)
+	}
+}
+
+func cloneSlice(s []bool) []bool {
+	c := make([]bool, len(s))
+
+	copy(c, s)
+
+	return c
 }
