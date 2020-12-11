fc794b4b80a83d2d7e4c381dd4dad42db0b697c3
Quentin Barrand <quentin@quba.fr>
2020-12-11 17:41:40 +0100

    Improve day 1.

--- 01/main.go
+++ 01/main.go
@@ -8,46 +8,48 @@ import (
 )
 
 func main() {
-	nums := make([]int, 0)
+	const (
+		notFound = -1
+		target   = 2020
+	)
 
-	var currentInt int
+	var (
+		currentInt int
+		m          = make(map[int]int)
+		part1      = notFound
+		part2      = notFound
+	)
 
-	for {
+	for i := 1; part1 == notFound || part2 == notFound; i++ {
 		if _, err := fmt.Scanf("%d", &currentInt); err != nil {
 			if !errors.Is(err, io.EOF) {
-				log.Fatalf("Error while reading the input: %v", err)
+				log.Fatalf("Line %d: %v", i, err)
 			}
 
 			break
 		}
 
-		nums = append(nums, currentInt)
-	}
+		v1 := target - currentInt
 
-	N := len(nums)
-
-	for i := 0; i < N-1; i++ {
-		for j := i + 1; j < N; j++ {
-			ni := nums[i]
-			nj := nums[j]
-
-			if ni+nj == 2020 {
-				log.Printf("Part one answer: %d", ni*nj)
-			}
+		if part1 == notFound && m[v1] > 0 {
+			part1 = v1 * currentInt
 		}
-	}
 
-	for i := 0; i < N-2; i++ {
-		for j := i + 1; j < N-1; j++ {
-			for k := j + 1; k < N; k++ {
-				ni := nums[i]
-				nj := nums[j]
-				nk := nums[k]
+		if part2 == notFound {
+			m[v1]--
 
-				if ni+nj+nk == 2020 {
-					log.Printf("Part two answer: %d", ni*nj*nk)
+			for k, v := range m {
+				if v2 := v1 - k; v > 0 && m[v2] > 0 {
+					part2 = v1 * v2 * currentInt
 				}
 			}
+
+			m[v1]++
 		}
+
+		m[currentInt]++
 	}
+
+	log.Printf("Part 1: %d", part1)
+	log.Printf("Part 2: %d", part2)
 }
