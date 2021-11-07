4651e583f7e5dc583f9aa9442091507bac5a23e3
Quentin Barrand <quentin@quba.fr>
2021-11-08 00:41:37 +0100

    Implement part 2.

--- 09/main.go
+++ 09/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 )
 
 const (
@@ -21,9 +22,9 @@ func main() {
 
 	part1 := NotFound
 
-	for {
-		var v int
+	var v int
 
+	for {
 		if _, err := fmt.Scanf("%d", &v); err != nil {
 			if !errors.Is(err, io.EOF) {
 				log.Fatalf("Line %d: %v", i, err)
@@ -57,5 +58,84 @@ func main() {
 		i++
 	}
 
-	log.Printf("Part 1: cannot sum to %d", part1)
+	log.Printf("Part 1: %d", part1)
+
+	part2 := NotFound
+
+	if _, err := os.Stdin.Seek(0, io.SeekStart); err != nil {
+		log.Fatalf("Failed to seek(0)")
+	}
+
+	sum := 0
+
+	r = ring.New(2)
+
+	for i := 0; i < 2; i++ {
+		if _, err := fmt.Scanf("%d", &v); err != nil {
+			if !errors.Is(err, io.EOF) {
+				log.Fatalf("Line %d: %v", i, err)
+			}
+
+			break
+		}
+
+		r.Value = v
+		r = r.Next()
+
+		sum += v
+	}
+
+	r = r.Prev()
+
+	for {
+		if sum == part1 {
+			min := r.Value.(int)
+
+			r.Do(func(i interface{}) {
+				if ii := i.(int); ii < min {
+					min = ii
+				}
+			})
+
+			max := r.Value.(int)
+
+			r.Do(func(i interface{}) {
+				if ii := i.(int); ii > max {
+					max = ii
+				}
+			})
+
+			part2 = min + max
+
+			break
+		}
+
+		if sum < part1 {
+			// let's read more data
+			if _, err := fmt.Scanf("%d", &v); err != nil {
+				if !errors.Is(err, io.EOF) {
+					log.Fatalf("Line %d: %v", i, err)
+				}
+
+				break
+			}
+
+			n := ring.New(1)
+			n.Value = v
+
+			// Add a link to the ring and set r to that link (most recent insertion)
+			r = r.Link(n).Prev()
+
+			sum += v
+
+			continue
+		}
+
+		if sum > part1 {
+			rem := r.Unlink(1).Value.(int)
+			sum -= rem
+		}
+	}
+
+	log.Printf("Part 2: %d", part2)
 }
